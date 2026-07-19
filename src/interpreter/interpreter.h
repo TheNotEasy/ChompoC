820b04cd178697d7c9a60698788703d262c9c01c
Yevdokimov Kirill <bony.toys@list.ru>
2026-07-19 21:02:09 +0700

    register terminal builtins in interpreter

--- src/interpreter/interpreter.h
+++ src/interpreter/interpreter.h
@@ -13,6 +13,7 @@
 
 class IOManager;
 class NetworkManager;
+class TerminalManager;
 class UserFunction;
 
 class Interpreter {
@@ -25,6 +26,7 @@ public:
     void install_io_builtins(IOManager &io_manager);
     void install_network_builtins(NetworkManager &network_manager);
     void install_secure_network_builtins(NetworkManager &network_manager);
+    void install_terminal_builtins(TerminalManager &terminal_manager);
     void install_system_builtins(std::vector<std::string> arguments);
     void interpret(const Program &program);
 
